77ac8614421a82d2b0a22b250812be7259e02d59
ok-olly <jeongoklee0108@gmail.com>
2023-04-30 18:04:12 +0900

    section 7 delete todos
    event.target.parentElement.remove()

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,24 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
 
+const deleteToDo = e => {
+  //   const li = e.target.parentElement;
+  //   li.remove();
+
+  e.target.parentElement.remove();
+};
+
 const paintToDo = newToDo => {
   const li = document.createElement('li');
   const span = document.createElement('span');
-  li.appendChild(span);
   span.innerText = newToDo;
+
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
+
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 };
 
