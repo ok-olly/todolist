2516e590631dc4b20f9d187c80beff2e52c111b6
ok-olly <jeongoklee0108@gmail.com>
2023-04-30 17:10:43 +0900

    section 6 background
    document.createElement() / document.body.appendChild()

--- js/quotes.js
+++ js/quotes.js
@@ -51,7 +51,7 @@ const quotes = [
 const quote = document.querySelector('#quote span:first-child');
 const author = document.querySelector('#quote span:last-child');
 
-const todaysQuote = quotes[Math.floor(Math.random() * (quotes.length + 1))];
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
 author.innerText = todaysQuote.author;
